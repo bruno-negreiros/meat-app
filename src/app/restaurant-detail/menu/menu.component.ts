ecf74e9934d4d5485c7835165cc09b6aac483e91
Bruno Ananias Negreiros <brunoananias_n@hotmail.com>
2019-10-26 19:27:54 -0400

    aplicando eventos no componente do carrinho de compras

--- src/app/restaurant-detail/menu/menu.component.ts
+++ src/app/restaurant-detail/menu/menu.component.ts
@@ -22,8 +22,4 @@ export class MenuComponent implements OnInit {
       .menuOfRestaurant(this.route.parent.snapshot.params['id']);
   }
 
-  addMenuItem(event: MenuItem){
-    this.shoppingCartService.addItem(event);
-  }
-
 }
